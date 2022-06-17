2ef175ef0542c6d316c1ef8e8cedfc1833f5d593
zephyrus21 <pandey.piyus@gmail.com>
2022-06-18 00:45:23 +0530

    added isAuth middleware

--- server/graphql/resolvers/post.js
+++ server/graphql/resolvers/post.js
@@ -10,5 +10,29 @@ module.exports = {
         throw new Error(error);
       }
     },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (!post) {
+          throw new Error("Post not found");
+        }
+        return post;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, ctx) {
+      const newPost = new Post({
+        body,
+      });
+      try {
+        const post = await newPost.save();
+        return post;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 };
