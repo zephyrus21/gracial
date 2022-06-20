56d04edf7a01828a3c061f96b216b23c3bb2fc27
zephyrus21 <pandey.piyus@gmail.com>
2022-06-20 21:37:52 +0530

    register page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,51 @@
-import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+  if (!loading) {
+    console.log(data.getPosts);
+  }
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <div>
+          {data.getPosts.map((post) => (
+            <div style={{ display: "flex" }} key={post.id}>
+              <p>{post.title}</p>
+              <p>{post.body}</p>
+              <p>{post.username}</p>
+              <p>{post.likeCount}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        body
+        createdAt
+        username
+      }
+    }
+  }
+`;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,110 @@
-import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const onChangeHandler = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const [adduser, { loading }] = useMutation(REGISTER_USER_MUTATION, {
+    update(_, data) {
+      setErrors({});
+      navigate("/");
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.errors);
+    },
+    variables: values,
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    adduser();
+  };
+
+  return (
+    <>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <div>
+          <form onSubmit={onSubmitHandler} noValidate>
+            <input
+              type='text'
+              name='username'
+              placeholder='Username'
+              value={values.username}
+              onChange={onChangeHandler}
+            />
+            {errors.username && <p>{errors.username}</p>}
+            <input
+              type='email'
+              name='email'
+              placeholder='Email'
+              value={values.email}
+              onChange={onChangeHandler}
+            />
+            {errors.email && <p>{errors.email}</p>}
+            <input
+              type='password'
+              name='password'
+              placeholder='Password'
+              value={values.password}
+              onChange={onChangeHandler}
+            />
+            {errors.password && <p>{errors.password}</p>}
+            <input
+              type='password'
+              name='confirmPassword'
+              placeholder='Confirm Password'
+              value={values.confirmPassword}
+              onChange={onChangeHandler}
+            />
+            {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+            <button type='submit'>Register</button>
+          </form>
+        </div>
+      )}
+    </>
+  );
 };
+
+const REGISTER_USER_MUTATION = gql`
+  mutation register(
+    $username: String!
+    $email: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      registerInput: {
+        username: $username
+        email: $email
+        password: $password
+        confirmPassword: $confirmPassword
+      }
+    ) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
